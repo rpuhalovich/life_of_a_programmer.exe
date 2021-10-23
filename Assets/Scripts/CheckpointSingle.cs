ab75c302c702d799125bc70dabf0873c1e80bdb2
Marvin Chew <marvin.chew@student.unimelb.edu.au>
2021-10-23 22:35:37 +1100

    For Master to fix

--- Assets/Scripts/CheckpointSingle.cs
+++ Assets/Scripts/CheckpointSingle.cs
@@ -4,32 +4,11 @@ using UnityEngine;
 
 public class CheckpointSingle : MonoBehaviour
 {
-    private LevelCheckpoints levelCheckpoints;
-    private MeshRenderer meshRenderer;
-
-    private void Awake() {
-        meshRenderer = GetComponent<MeshRenderer>();
-    }
-
-    private void Start() {
-        Show();
-    }
+    [SerializeField] private Material triggered;
 
     private void OnTriggerEnter(Collider other) {
-        if (other.TryGetComponent<PlayerController>(out PlayerController player )) {
-            levelCheckpoints.PlayerThroughCheckpoint(this);
+        if (other.TryGetComponent<PlayerController>(out PlayerController player)) {
+            Debug.Log("Checkpoint!");
         }
-    }
-
-    public void setTrackCheckpoints(LevelCheckpoints levelCheckpoints) {
-        this.levelCheckpoints = levelCheckpoints;
-    }
-
-    public void Show() {
-        meshRenderer.enabled = true;
-    }
-
-    public void Hide() {
-        meshRenderer.enabled = false;
     }
 }
