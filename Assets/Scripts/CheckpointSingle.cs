78e1c509a92621f6bb2b354c83cf2a69acb8683b
Marvin Chew <marvin.chew@student.unimelb.edu.au>
2021-10-24 00:54:23 +1100

    Start stopwatch on first checkpoint, stop on last

--- Assets/Scripts/CheckpointSingle.cs
+++ Assets/Scripts/CheckpointSingle.cs
@@ -6,11 +6,18 @@ public class CheckpointSingle : MonoBehaviour
 {
     [SerializeField] private Material triggered;
 
+    private LevelCheckpoints levelCheckpoints;
+
     private void OnTriggerEnter(Collider other) {
         if (other.TryGetComponent<PlayerController>(out PlayerController player)) {
             foreach (Transform child in transform) {
                 child.GetComponent<MeshRenderer>().material = triggered;
+                levelCheckpoints.PlayerThroughCheckpoint(this);
             }
         }
+    }
+
+    public void SetLevelCheckpoints(LevelCheckpoints levelCheckpoints) {
+        this.levelCheckpoints = levelCheckpoints;
     }
 }
