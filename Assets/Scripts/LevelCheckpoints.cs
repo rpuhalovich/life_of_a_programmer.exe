22b1808090ed673bdc7b61a2889ddc2e9f48d8da
Marvin Chew <marvin.chew@student.unimelb.edu.au>
2021-10-30 23:38:26 +1100

    Collect all checkpoints to finish level

--- Assets/Scripts/LevelCheckpoints.cs
+++ Assets/Scripts/LevelCheckpoints.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class LevelCheckpoints : MonoBehaviour
 {
     private List<CheckpointSingle> checkpointSingles;
+    private int nextCheckpointSingleIndex;
     [SerializeField] private Stopwatch stopwatch;
+    [SerializeField] private Material triggered;
+    [SerializeField] private Transform respawnPoint;
 
     private void Awake()
     {
@@ -18,15 +21,28 @@ public class LevelCheckpoints : MonoBehaviour
             checkpointSingle.SetLevelCheckpoints(this);
             checkpointSingles.Add(checkpointSingle);
         }
+
+        nextCheckpointSingleIndex = 0;
     }
 
     public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle)
     {
+        if (checkpointSingles.IndexOf(checkpointSingle) == nextCheckpointSingleIndex) {
+            // Set light beams to green.
+            foreach (Transform child in checkpointSingle.transform) {
+                child.GetComponent<MeshRenderer>().material = triggered;
+            }
+            // Set respawn point to this checkpoint.
+            respawnPoint.transform.position = checkpointSingle.transform.position;
+            nextCheckpointSingleIndex++;
+        }
+
         if (checkpointSingles.IndexOf(checkpointSingle) == 0)
         {
             stopwatch.StartStopwatch();
         }
-        else if (checkpointSingles.IndexOf(checkpointSingle) == checkpointSingles.Count - 1)
+
+        if (nextCheckpointSingleIndex == checkpointSingles.Count)
         {
             stopwatch.StopStopwatch();
         }
