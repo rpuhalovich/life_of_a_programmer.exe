692ced4fdd93a5aa131bf11738c681c8ee07dbae
rpuhalovich <reanpuhalovich@gmail.com>
2021-11-01 08:18:57 +1000

    Nothing's wrong with the music. Updated default timer value.

--- Assets/Scripts/Stopwatch.cs
+++ Assets/Scripts/Stopwatch.cs
@@ -19,7 +19,7 @@ public class Stopwatch : MonoBehaviour
 
         // Get best time from playerprefs for the current scene and apply it to the best time text.
         scene = SceneManager.GetActiveScene();
-        bestTimeValue = PlayerPrefs.GetFloat(scene.name, 6039000.0f); // If no best time set to 30 minutes.
+        bestTimeValue = PlayerPrefs.GetFloat(scene.name, 3599.999f); // If no best time set to 30 minutes.
         TimeSpan time = TimeSpan.FromSeconds(bestTimeValue);
         bestTimeText.text = time.ToString(@"mm\:ss\:fff");
     }
